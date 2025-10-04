bbc42c26a1e2a1a22191c4a60fcf4011e08e8a13
Tsue_Hic22009 <hic22009@byui.edu>
2025-10-04 17:35:03 -0500

    i copied the code from pgadmin into the database folder and named it db-sql-code.sql

---
+++ database/db-sql-code.sql
@@ -0,0 +1,3 @@
+CREATE TYPE public.account_type AS ENUM ('Client', 'Employee', 'Admin');
+
+ALTER TYPE public.account_type OWNER TO hickm76907;
--- server.js
+++ server.js
@@ -16,14 +16,18 @@ const static = require("./routes/static")
  *************************/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
-app.set("layout", "./layouts/layout") 
+app.set("layout", "./layouts/layout")
+
 
 /* ***********************
  * Routes
  *************************/
-app.get("/", function(req, res) {
-  res.render("index", { title: "Home" })
+app.use(static)
+//Index Route
+app.get("/", function(req,res) {
+  res.render("index", {title: "Home"})
 })
+
 
 /* ***********************
  * Local Server Information
