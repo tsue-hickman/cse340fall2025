cb0d80fdc3e504d0bc803ddd446daec018532349
Tsue_Hic22009 <hic22009@byui.edu>
2025-10-11 19:47:46 -0500

    updated the server.js for the base controller usage

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
 
 /* ***********************
  * View Engine and Templates
@@ -24,9 +25,8 @@ app.set("layout", "./layouts/layout")
  *************************/
 app.use(static)
 //Index Route
-app.get("/", function(req,res) {
-  res.render("index", {title: "Home"})
-})
+//Index Route
+app.get("/", baseController.buildHome)
 
 /* ***********************
  * Local Server Information
