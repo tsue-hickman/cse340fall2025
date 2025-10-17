d2416682d9461a5dc51b5948899da48ae49ac776
Tsue_Hic22009 <hic22009@byui.edu>
2025-10-16 21:59:21 -0500

    new route for the vehicle detail view

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -6,4 +6,17 @@ const invController = require("../controllers/invController")
 // Route to build inventory by classification view
 router.get("/type/:classificationId", invController.buildByClassificationId);
 
+// Needed Resources 
+const express = require("express")
+const router = new express.Router() 
+const invController = require("../controllers/invController")
+
+// Route to build inventory by classification view
+router.get("/type/:classificationId", invController.buildByClassificationId);
+
+// Route to build inventory item detail view
+router.get("/detail/:invId", invController.buildByInventoryId);
+
+module.exports = router;
+
 module.exports = router;
