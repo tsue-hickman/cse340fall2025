0c0bad787197b06b9707519b8364f9702a03a718
Tsue_Hic22009 <hic22009@byui.edu>
2025-10-17 19:07:07 -0500

    updated invController for last task on task 2 of this weeks assignment (wk5)

--- controllers/invController.js
+++ controllers/invController.js
@@ -1,12 +1,13 @@
 const invModel = require("../models/inventory-model")
 const utilities = require("../utilities/")
+const errorHandler = require("../middleware/errorHandler")
 
 const invCont = {}
 
 /* ***************************
  *  Build inventory by classification view
  * ************************** */
-invCont.buildByClassificationId = async function (req, res, next) {
+invCont.buildByClassificationId = errorHandler.errorHandler(async function (req, res, next) {
   const classification_id = req.params.classificationId
   const data = await invModel.getInventoryByClassificationId(classification_id)
   const grid = await utilities.buildClassificationGrid(data)
@@ -17,12 +18,12 @@ invCont.buildByClassificationId = async function (req, res, next) {
     nav,
     grid,
   })
-}
+})
 
 /* ***************************
  *  Build inventory item detail view
  * ************************** */
-invCont.buildByInventoryId = async function (req, res, next) {
+invCont.buildByInventoryId = errorHandler.errorHandler(async function (req, res, next) {
   const inv_id = req.params.invId
   const data = await invModel.getInventoryByInventoryId(inv_id)
   const detail = await utilities.buildDetailView(data)
@@ -33,6 +34,6 @@ invCont.buildByInventoryId = async function (req, res, next) {
     nav,
     detail,
   })
-}
+})
 
 module.exports = invCont
