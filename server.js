1d402a62f587870e116fa860e493bb4b9a22d66c
Tsue_Hic22009 <hic22009@byui.edu>
2025-10-20 09:58:56 -0500

    updated the server showing the "undefined" for the port in my server.js

--- server.js
+++ server.js
@@ -46,8 +46,8 @@ app.use(errorHandler.handleErrors)
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT || 5500
+const host = process.env.HOST || 'localhost'
 
 /* ***********************
  * Log statement to confirm server operation
