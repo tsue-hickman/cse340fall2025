8b719fd6f6f1ff39e561595474bf626da918684c
Tsue_Hic22009 <hic22009@byui.edu>
2025-11-08 16:08:33 -0600

    update controller with management functions

--- controllers/invController.js
+++ controllers/invController.js
@@ -37,6 +37,127 @@ invCont.buildByInventoryId = errorHandler.errorHandler(async function (req, res,
 })
 
 /* ***************************
+ *  Build inventory management view
+ * ************************** */
+invCont.buildManagement = errorHandler.errorHandler(async function (req, res, next) {
+  let nav = await utilities.getNav()
+  res.render("./inventory/management", {
+    title: "Vehicle Management",
+    nav,
+    errors: null,
+  })
+})
+
+/* ***************************
+ *  Build add classification view
+ * ************************** */
+invCont.buildAddClassification = errorHandler.errorHandler(async function (req, res, next) {
+  let nav = await utilities.getNav()
+  res.render("./inventory/add-classification", {
+    title: "Add New Classification",
+    nav,
+    errors: null,
+  })
+})
+
+/* ***************************
+ *  Process add classification
+ * ************************** */
+invCont.addClassification = errorHandler.errorHandler(async function (req, res, next) {
+  let nav = await utilities.getNav()
+  const { classification_name } = req.body
+
+  const addResult = await invModel.addClassification(classification_name)
+
+  if (addResult) {
+    req.flash(
+      "notice",
+      `The ${classification_name} classification was successfully added.`
+    )
+    res.status(201).render("./inventory/management", {
+      title: "Vehicle Management",
+      nav,
+      errors: null,
+    })
+  } else {
+    req.flash("notice", "Sorry, adding the classification failed.")
+    res.status(501).render("./inventory/add-classification", {
+      title: "Add New Classification",
+      nav,
+      errors: null,
+    })
+  }
+})
+
+/* ***************************
+ *  Build add inventory view
+ * ************************** */
+invCont.buildAddInventory = errorHandler.errorHandler(async function (req, res, next) {
+  let nav = await utilities.getNav()
+  let classificationList = await utilities.buildClassificationList()
+  res.render("./inventory/add-inventory", {
+    title: "Add New Vehicle",
+    nav,
+    classificationList,
+    errors: null,
+  })
+})
+
+/* ***************************
+ *  Process add inventory
+ * ************************** */
+invCont.addInventory = errorHandler.errorHandler(async function (req, res, next) {
+  let nav = await utilities.getNav()
+  let classificationList = await utilities.buildClassificationList()
+  
+  const {
+    inv_make,
+    inv_model,
+    inv_year,
+    inv_description,
+    inv_image,
+    inv_thumbnail,
+    inv_price,
+    inv_miles,
+    inv_color,
+    classification_id
+  } = req.body
+
+  const addResult = await invModel.addInventory(
+    inv_make,
+    inv_model,
+    inv_year,
+    inv_description,
+    inv_image,
+    inv_thumbnail,
+    inv_price,
+    inv_miles,
+    inv_color,
+    classification_id
+  )
+
+  if (addResult) {
+    req.flash(
+      "notice",
+      `The ${inv_year} ${inv_make} ${inv_model} was successfully added.`
+    )
+    res.status(201).render("./inventory/management", {
+      title: "Vehicle Management",
+      nav,
+      errors: null,
+    })
+  } else {
+    req.flash("notice", "Sorry, adding the vehicle failed.")
+    res.status(501).render("./inventory/add-inventory", {
+      title: "Add New Vehicle",
+      nav,
+      classificationList,
+      errors: null,
+    })
+  }
+})
+
+/* ***************************
  *  Intentionally trigger a 500 error
  * ************************** */
 invCont.triggerError = errorHandler.errorHandler(async function (req, res, next) {
