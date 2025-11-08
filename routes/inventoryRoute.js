25bf560c02dfca2980c1639aadd0687805c0b6d9
Tsue_Hic22009 <hic22009@byui.edu>
2025-11-08 16:08:26 -0600

    add routes for inventory management

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,6 +2,7 @@
 const express = require("express")
 const router = new express.Router() 
 const invController = require("../controllers/invController")
+const invValidate = require('../utilities/inventory-validation')
 
 // Route to build inventory by classification view
 router.get("/type/:classificationId", invController.buildByClassificationId);
@@ -11,5 +12,30 @@ router.get("/detail/:invId", invController.buildByInventoryId);
 
 // Route to trigger intentional error (for testing)
 router.get("/trigger-error", invController.triggerError);
+
+// Route to show inventory management view
+router.get("/", invController.buildManagement);
+
+// Route to show add classification view
+router.get("/add-classification", invController.buildAddClassification);
+
+// Route to process add classification
+router.post(
+  "/add-classification",
+  invValidate.classificationRules(),
+  invValidate.checkClassificationData,
+  invController.addClassification
+);
+
+// Route to show add inventory view
+router.get("/add-inventory", invController.buildAddInventory);
+
+// Route to process add inventory
+router.post(
+  "/add-inventory",
+  invValidate.inventoryRules(),
+  invValidate.checkInventoryData,
+  invController.addInventory
+);
 
 module.exports = router;
