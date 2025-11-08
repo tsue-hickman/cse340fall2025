59c123e348500baa0bd3e18dd4664be7ac18f68f
Tsue_Hic22009 <hic22009@byui.edu>
2025-11-08 16:08:19 -0600

    add classification dropdown builder

--- utilities/index.js
+++ utilities/index.js
@@ -1,6 +1,5 @@
 const invModel = require("../models/inventory-model")
 const Util = {}
-const errorHandler = require("../middleware/errorHandler") 
 
 /* ************************
  * Constructs the nav HTML unordered list
@@ -78,10 +77,32 @@ Util.buildDetailView = async function(vehicle){
 }
 
 /* ****************************************
+* Build classification list (dropdown)
+**************************************** */
+Util.buildClassificationList = async function (classification_id = null) {
+  let data = await invModel.getClassifications()
+  let classificationList =
+    '<select name="classification_id" id="classificationList" required>'
+  classificationList += "<option value=''>Choose a Classification</option>"
+  data.rows.forEach((row) => {
+    classificationList += '<option value="' + row.classification_id + '"'
+    if (
+      classification_id != null &&
+      row.classification_id == classification_id
+    ) {
+      classificationList += " selected "
+    }
+    classificationList += ">" + row.classification_name + "</option>"
+  })
+  classificationList += "</select>"
+  return classificationList
+}
+
+/* ****************************************
  * Middleware For Handling Errors
  * Wrap other function in this for 
  * General Error Handling
  **************************************** */
-Util.handleErrors = errorHandler.errorHandler
+Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
 
 module.exports = Util
